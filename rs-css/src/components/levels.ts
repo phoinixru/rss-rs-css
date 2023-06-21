53e941eba4e33d0b20e7af8d127019693e1e5c97
Vitaly Kukushkin <phoinixru@gmail.com>
2023-06-21 17:29:18 +0200

    feat: show level list

--- rs-css/src/components/levels.ts
+++ rs-css/src/components/levels.ts
@@ -5,9 +5,10 @@ import Component from './component';
 const CssClasses = {
   LEVELS: 'levels',
   LIST: 'level-list',
-  LEVEL: 'level-item',
-  DONE: 'level-item--done',
-  HELPED: 'level-item--helped',
+  LEVEL: 'level',
+  DONE: 'level--done',
+  HELPED: 'level--helped',
+  CURRENT: 'level--current',
 };
 
 export default class LevelList extends Component {
@@ -20,15 +21,50 @@ export default class LevelList extends Component {
 
     this.#levels = levels;
     this.render();
+    this.addEventListeners();
+  }
+
+  private addEventListeners(): void {
+    this.element.addEventListener('click', (event) => this.handleClicks(event));
+  }
+
+  private handleClicks(event: MouseEvent): void {
+    const { target } = event;
+    if (!(target instanceof HTMLElement)) {
+      return;
+    }
+
+    const item = target.closest(`.${CssClasses.LEVEL}`);
+    if (!(item instanceof HTMLElement)) {
+      return;
+    }
+
+    const id = Number(item.dataset.index);
+    this.changeLevel(id);
   }
 
   public changeLevel(level: number): void {
     this.#currentLevel = level;
+    this.render();
   }
 
   private render(): void {
     const list = elt<HTMLOListElement>('ol', { className: CssClasses.LIST });
 
+    this.#levels.forEach((level, index) => {
+      const { syntax } = level;
+      const listItem = elt<HTMLLIElement>('li', { className: CssClasses.LEVEL });
+      listItem.innerHTML = syntax;
+      listItem.dataset.index = String(index);
+
+      list.append(listItem);
+
+      if (index === this.#currentLevel) {
+        listItem.classList.add(CssClasses.CURRENT);
+      }
+    });
+
+    this.element.innerHTML = '';
     this.element.append('Levels:', list);
   }
 }
